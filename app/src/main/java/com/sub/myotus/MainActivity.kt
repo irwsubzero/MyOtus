c08ad74d51e485f26e5bb5ff0b2d45e1d9bba16b
irwsubzero <irwsubzero@gmail.com>
2020-10-19 10:40:13 +0300

    Верстка первая предварительная

--- app/src/main/java/com/sub/myotus/MainActivity.kt
+++ app/src/main/java/com/sub/myotus/MainActivity.kt
@@ -2,10 +2,22 @@ package com.sub.myotus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnFilm1.setOnClickListener{
+            imgView1.setBackgroundColor(12)
+        }
+
+
     }
 }
+
+
+
+
